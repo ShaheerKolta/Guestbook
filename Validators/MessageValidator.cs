f51ae00f22d6d4fe4c3224b17e4f78ea8fff4872
Shaheer Kolta <shaheermehany@gmail.com>
2022-08-12 15:29:19 +0200

    created Message Validator

---
+++ Validators/MessageValidator.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+using Guestbook.Model;
+
+namespace Guestbook.Validators
+{
+    public class MessageValidator : AbstractValidator<Message>
+    {
+        public MessageValidator()
+        {
+            RuleFor(message => message.Message_Id).NotNull();
+            RuleFor(message => message.User_Id).NotNull();
+            RuleFor(message => message.Message_Content).NotNull().MinimumLength(2);
+        }
+    }
+}
