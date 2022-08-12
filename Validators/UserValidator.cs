0f44ce9d0cecfcee3a317c7f1b49ccc042456277
Shaheer Kolta <shaheermehany@gmail.com>
2022-08-12 15:33:30 +0200

    Created UserValidator

---
+++ Validators/UserValidator.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+using Guestbook.Model;
+
+namespace Guestbook.Validators
+{
+    public class UserValidator : AbstractValidator<User>
+    {
+        public UserValidator()
+        {
+            RuleFor(user => user.Name).NotNull();
+            RuleFor(user => user.Date_of_Birth).NotNull();
+            RuleFor(user => user.Email).NotNull().EmailAddress();
+            RuleFor(user => user.Password).NotNull().MinimumLength(4).MaximumLength(10);
+        }
+    }
+}
