1eee691587e60aaa9260dc61d1664922f021b116
Shaheer Kolta <shaheermehany@gmail.com>
2022-08-14 13:35:49 +0200

    Added Authentication and Authorization with fallback policy in program.cs

--- Program.cs
+++ Program.cs
@@ -1,6 +1,9 @@
 using Guestbook.Dapper;
 using Guestbook.Interfaces;
 using Guestbook.Repositories;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -14,6 +17,37 @@ builder.Services.AddSingleton<DapperContext>();
 builder.Services.AddScoped<IUserRepository, UserRepository>();
 builder.Services.AddScoped<IMessageRepository, MessageRepository>();
 
+builder.Services.AddAuthorization(opt =>
+{
+
+    opt.AddPolicy("Admin", p =>
+    {
+        p.RequireClaim("Role", "Admin");
+    });
+
+    opt.AddPolicy("User", p =>
+    {
+        p.RequireClaim("Role", "User", "Admin");
+    });
+    opt.FallbackPolicy = new AuthorizationPolicyBuilder().RequireAuthenticatedUser().Build();
+});
+
+
+builder.Services.AddAuthentication("Bearer").AddJwtBearer(opt =>
+{
+    opt.TokenValidationParameters = new()
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateIssuerSigningKey = true,
+        ValidIssuer = builder.Configuration.GetValue<string>("Authentication:Issuer"),
+        ValidAudience = builder.Configuration.GetValue<string>("Authentication:Audience"),
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(
+            builder.Configuration.GetValue<string>("Authentication:SecretKey")))
+    };
+});
+
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -24,6 +58,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseAuthentication();
 
 app.UseAuthorization();
 
